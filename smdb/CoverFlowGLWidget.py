f4ab62237822cbd604c17011f0425d1b5eb0c1d8
Scott Cegielski <scegielski@yahoo.com>
2025-10-26 18:37:54 -0700

    Covers flow motion goverened

--- smdb/CoverFlowGLWidget.py
+++ smdb/CoverFlowGLWidget.py
@@ -6,8 +6,15 @@ from OpenGL.GLU import *
 import numpy as np
 
 class CoverFlowGLWidget(QOpenGLWidget):
+    def _init_anim_queue(self):
+        self._anim_queue = []
+
+    def _store_prev_cover(self):
+        self._prev_cover_image = self.cover_image
+        self._prev_texture_id = self.texture_id
+
     def animate_cover_transition(self, direction):
-        # direction: +1 for next (from bottom), -1 for previous (from top)
+        self._store_prev_cover()
         self._anim_direction = direction
         self._anim_progress = 0.0
         self._animating = True
@@ -16,27 +23,36 @@ class CoverFlowGLWidget(QOpenGLWidget):
 
     def timerEvent(self, event):
         if getattr(self, '_animating', False):
-            # 3x slower: 0.08 / 3 = ~0.027
             self._anim_progress += 0.027
             if self._anim_progress >= 1.0:
                 self._anim_progress = 1.0
                 self._animating = False
                 self.killTimer(self._anim_timer)
+                self._prev_cover_image = None
+                self._prev_texture_id = None
+                self._process_anim_queue()  # Start next animation if queued
             self.update()
     wheelMovieChange = pyqtSignal(int)  # +1 for next, -1 for previous
     def wheelEvent(self, event):
-        # Accumulate wheel delta and emit for every full notch (±120)
+        # Accumulate wheel delta and queue events for consistent animation speed
         if not hasattr(self, '_wheel_accum'):
             self._wheel_accum = 0
         self._wheel_accum += event.angleDelta().y()
         while self._wheel_accum >= 120:
-            self.animate_cover_transition(-1)
-            self.wheelMovieChange.emit(-1)
+            self._anim_queue.append(-1)
             self._wheel_accum -= 120
         while self._wheel_accum <= -120:
-            self.animate_cover_transition(1)
-            self.wheelMovieChange.emit(1)
+            self._anim_queue.append(1)
             self._wheel_accum += 120
+        self._process_anim_queue()
+
+    def _process_anim_queue(self):
+        if getattr(self, '_animating', False):
+            return  # Wait for current animation to finish
+        if self._anim_queue:
+            direction = self._anim_queue.pop(0)
+            self.animate_cover_transition(direction)
+            self.wheelMovieChange.emit(direction)
     def __init__(self, parent=None):
         super().__init__(parent)
         # Enable sample buffers for anti-aliasing
@@ -48,6 +64,7 @@ class CoverFlowGLWidget(QOpenGLWidget):
         self.y_rotation = 0.0
         self.last_mouse_x = None
         self.aspect_ratio = 1.0
+        self._init_anim_queue()
 
     def set_cover_image(self, image_path):
         self.cover_image = QImage(image_path)
@@ -90,34 +107,74 @@ class CoverFlowGLWidget(QOpenGLWidget):
         import math
         fov_y = 30.0
         z = (quad_h / 2) / math.tan(math.radians(fov_y / 2))
-        # Animation: slide in from top or bottom
-        y_offset = 0.0
-        if getattr(self, '_animating', False):
+        # Animation: dual covers
+        if getattr(self, '_animating', False) and self._prev_cover_image is not None:
             progress = self._anim_progress
-            # Ease-in-out using smoothstep
             smooth_progress = progress * progress * (3 - 2 * progress)
             direction = getattr(self, '_anim_direction', 1)
-            # Slide from top (-1) or bottom (+1)
+            # Outgoing cover offset
             if direction == 1:
-                y_offset = (1.0 - smooth_progress) * 2.0  # start below, move up
+                prev_y_offset = -(smooth_progress) * 2.0  # move up (outgoing goes up)
+                curr_y_offset = (1.0 - smooth_progress) * 2.0  # move up from below (incoming comes up)
             else:
-                y_offset = -(1.0 - smooth_progress) * 2.0  # start above, move down
-        glTranslatef(0.0, y_offset, -z)
-        glRotatef(self.y_rotation, 0.0, 1.0, 0.0)
-        if self.cover_image and not self.cover_image.isNull():
-            if self.texture_id is None:
-                self.texture_id = self.createTextureFromQImage(self.cover_image)
-            glBindTexture(GL_TEXTURE_2D, self.texture_id)
-            glBegin(GL_QUADS)
-            glTexCoord2f(0.0, 1.0)
-            glVertex3f(-quad_w/2, -quad_h/2, 0.0)
-            glTexCoord2f(1.0, 1.0)
-            glVertex3f(quad_w/2, -quad_h/2, 0.0)
-            glTexCoord2f(1.0, 0.0)
-            glVertex3f(quad_w/2, quad_h/2, 0.0)
-            glTexCoord2f(0.0, 0.0)
-            glVertex3f(-quad_w/2, quad_h/2, 0.0)
-            glEnd()
+                prev_y_offset = smooth_progress * 2.0  # move down (outgoing goes down)
+                curr_y_offset = -(1.0 - smooth_progress) * 2.0  # move down from above (incoming comes down)
+            # Draw previous cover
+            glPushMatrix()
+            glTranslatef(0.0, prev_y_offset, -z)
+            glRotatef(self.y_rotation, 0.0, 1.0, 0.0)
+            if self._prev_texture_id is None and self._prev_cover_image and not self._prev_cover_image.isNull():
+                self._prev_texture_id = self.createTextureFromQImage(self._prev_cover_image)
+            if self._prev_texture_id:
+                glBindTexture(GL_TEXTURE_2D, self._prev_texture_id)
+                glBegin(GL_QUADS)
+                glTexCoord2f(0.0, 1.0)
+                glVertex3f(-quad_w/2, -quad_h/2, 0.0)
+                glTexCoord2f(1.0, 1.0)
+                glVertex3f(quad_w/2, -quad_h/2, 0.0)
+                glTexCoord2f(1.0, 0.0)
+                glVertex3f(quad_w/2, quad_h/2, 0.0)
+                glTexCoord2f(0.0, 0.0)
+                glVertex3f(-quad_w/2, quad_h/2, 0.0)
+                glEnd()
+            glPopMatrix()
+            # Draw current cover
+            glPushMatrix()
+            glTranslatef(0.0, curr_y_offset, -z)
+            glRotatef(self.y_rotation, 0.0, 1.0, 0.0)
+            if self.cover_image and not self.cover_image.isNull():
+                if self.texture_id is None:
+                    self.texture_id = self.createTextureFromQImage(self.cover_image)
+                glBindTexture(GL_TEXTURE_2D, self.texture_id)
+                glBegin(GL_QUADS)
+                glTexCoord2f(0.0, 1.0)
+                glVertex3f(-quad_w/2, -quad_h/2, 0.0)
+                glTexCoord2f(1.0, 1.0)
+                glVertex3f(quad_w/2, -quad_h/2, 0.0)
+                glTexCoord2f(1.0, 0.0)
+                glVertex3f(quad_w/2, quad_h/2, 0.0)
+                glTexCoord2f(0.0, 0.0)
+                glVertex3f(-quad_w/2, quad_h/2, 0.0)
+                glEnd()
+            glPopMatrix()
+        else:
+            # Normal render (single cover)
+            glTranslatef(0.0, 0.0, -z)
+            glRotatef(self.y_rotation, 0.0, 1.0, 0.0)
+            if self.cover_image and not self.cover_image.isNull():
+                if self.texture_id is None:
+                    self.texture_id = self.createTextureFromQImage(self.cover_image)
+                glBindTexture(GL_TEXTURE_2D, self.texture_id)
+                glBegin(GL_QUADS)
+                glTexCoord2f(0.0, 1.0)
+                glVertex3f(-quad_w/2, -quad_h/2, 0.0)
+                glTexCoord2f(1.0, 1.0)
+                glVertex3f(quad_w/2, -quad_h/2, 0.0)
+                glTexCoord2f(1.0, 0.0)
+                glVertex3f(quad_w/2, quad_h/2, 0.0)
+                glTexCoord2f(0.0, 0.0)
+                glVertex3f(-quad_w/2, quad_h/2, 0.0)
+                glEnd()
 
     def createTextureFromQImage(self, qimage):
         qimage = qimage.convertToFormat(QImage.Format_RGBA8888)
